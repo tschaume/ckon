08d5c05e223fe20325e135df092de32b55c73086
Patrick Huck <phuck@lbl.gov>
2013-02-07 02:17:13 -0800

    fix ckon.cfg, add default_value's

--- cmdline.cc
+++ cmdline.cc
@@ -40,26 +40,21 @@ void cmdline::runSetup() {
   fs::ofstream cfgfile(ckon_config_file);
   Tee tee( std::cout, cfgfile );
   TeeStream both( tee );
-  both << "----------------------------" << endl;
-  both << "default directory structure:" << endl;
-  both << "----------------------------" << endl;
-  both << "ckon_src_dir : " << ckon_src_dir << endl;
-  both << "ckon_core_dir : " << ckon_core_dir << endl;
-  both << "ckon_obsolete_dir : " << ckon_obsolete_dir << endl;
-  both << "ckon_prog_subdir : " << ckon_prog_subdir << endl;
-  both << "ckon_macro_subdir : " << ckon_macro_subdir << endl;
-  both << "ckon_build_dir : " << ckon_build_dir << endl;
-  both << "ckon_install_dir : " << ckon_install_dir << endl;
-  both << "ckon_exclSuffix : " << ckon_exclSuffix << endl;
-  both << "ckon_DontScan : " << ckon_DontScan << endl;
-  both << "ckon_NoRootCint : " << ckon_NoRootCint << endl;
-  both << "----------------------" << endl;
-  both << "default build options:" << endl;
-  both << "----------------------" << endl;
-  both << "pythia : " << bPythia << endl;
-  both << "roofit : " << bRooFit << endl;
-  both << "boost : " << bBoost << endl;
-  both << "suffix : " << bSuffix << endl;
+  both << "pythia=" << bPythia << endl;
+  both << "roofit=" << bRooFit << endl;
+  both << "boost=" << bBoost << endl;
+  both << "suffix=" << bSuffix << endl;
+  both << "[ckon]" << endl;
+  both << "src_dir=" << ckon_src_dir << endl;
+  both << "core_dir=" << ckon_core_dir << endl;
+  both << "obsolete_dir=" << ckon_obsolete_dir << endl;
+  both << "prog_subdir=" << ckon_prog_subdir << endl;
+  both << "macro_subdir=" << ckon_macro_subdir << endl;
+  both << "build_dir=" << ckon_build_dir << endl;
+  both << "install_dir=" << ckon_install_dir << endl;
+  both << "exclSuffix=\"" << ckon_exclSuffix << "\"" << endl;
+  both << "DontScan=\"" << ckon_DontScan << "\"" << endl;
+  both << "NoRootCint=" << ckon_NoRootCint << endl;
   both.close();
 
   ut->writeConfigureAc();
@@ -75,25 +70,25 @@ bool cmdline::parse(int argc, char *argv[]) {
     ("help,h" , po::bool_switch(&bHelp)    , "show this help") 
     ("verbose,v" , po::bool_switch(&bVerbose) , "verbose output")
     (",j" , po::value<int>(&nCpu) , "call make with option -j <#cores> (parallel compile)")
-    ("ckon_cmd" , po::value<string>(&ckon_cmd)  , "<none> | setup | clean | install");
+    ("ckon_cmd" , po::value<string>(&ckon_cmd)->default_value("")  , "<none> | setup | clean | install");
 
   po::options_description config("Configuration"); 
   config.add_options() 
-    ("pythia,p"  , po::bool_switch(&bPythia)  , "link with pythia library")
-    ("roofit,r"  , po::bool_switch(&bRooFit)  , "link with roofit library")
-    ("suffix,s"  , po::bool_switch(&bSuffix)  , "Add suffix + in LinkDef file")
-    ("boost,b"   , po::bool_switch(&bBoost)   , "include BOOST_INC and BOOST_LIB")
-    ("ckon_config_file"  , po::value<string>(&ckon_config_file)  , "config file name")
-    ("ckon_src_dir"      , po::value<string>(&ckon_src_dir)      , "source dir")
-    ("ckon_core_dir"     , po::value<string>(&ckon_core_dir)     , "core dir")
-    ("ckon_obsolete_dir" , po::value<string>(&ckon_obsolete_dir) , "obsolete dir")
-    ("ckon_exclSuffix"   , po::value<string>(&ckon_exclSuffix)   , "exclude dirs from suffix")
-    ("ckon_DontScan"     , po::value<string>(&ckon_DontScan)     , "omit dirs from source scan")
-    ("ckon_NoRootCint"   , po::value<string>(&ckon_NoRootCint)   , "don't generate dictionary for libraries")
-    ("ckon_prog_subdir"  , po::value<string>(&ckon_prog_subdir)  , "programs source dir")
-    ("ckon_macro_subdir" , po::value<string>(&ckon_macro_subdir) , "macros subdir")
-    ("ckon_build_dir"    , po::value<string>(&ckon_build_dir)    , "build dir")
-    ("ckon_install_dir"  , po::value<string>(&ckon_install_dir)  , "install dir");
+    ("pythia,p"  , po::value<bool>(&bPythia)->default_value(0)  , "link with pythia library")
+    ("roofit,r"  , po::value<bool>(&bRooFit)->default_value(0)  , "link with roofit library")
+    ("suffix,s"  , po::value<bool>(&bSuffix)->default_value(0)  , "Add suffix + in LinkDef file")
+    ("boost,b"   , po::value<bool>(&bBoost)->default_value(0)   , "include BOOST_INC and BOOST_LIB")
+    ("ckon.config_file"  , po::value<string>(&ckon_config_file)->default_value(ckon_config_file), "config file name")
+    ("ckon.src_dir"      , po::value<string>(&ckon_src_dir)->default_value(ckon_src_dir), "source dir")
+    ("ckon.core_dir"     , po::value<string>(&ckon_core_dir)->default_value(ckon_core_dir), "core dir")
+    ("ckon.obsolete_dir" , po::value<string>(&ckon_obsolete_dir)->default_value(ckon_obsolete_dir), "obsolete dir")
+    ("ckon.exclSuffix"   , po::value<string>(&ckon_exclSuffix)->default_value(ckon_exclSuffix), "exclude dirs from suffix")
+    ("ckon.DontScan"     , po::value<string>(&ckon_DontScan)->default_value(ckon_DontScan), "omit dirs from source scan")
+    ("ckon.NoRootCint"   , po::value<string>(&ckon_NoRootCint)->default_value(ckon_NoRootCint), "don't generate dictionary for libraries")
+    ("ckon.prog_subdir"  , po::value<string>(&ckon_prog_subdir)->default_value(ckon_prog_subdir), "programs source dir")
+    ("ckon.macro_subdir" , po::value<string>(&ckon_macro_subdir)->default_value(ckon_macro_subdir), "macros subdir")
+    ("ckon.build_dir"    , po::value<string>(&ckon_build_dir)->default_value(ckon_build_dir), "build dir")
+    ("ckon.install_dir"  , po::value<string>(&ckon_install_dir)->default_value(ckon_install_dir), "install dir");
 
   po::options_description allopts; 
   allopts.add(generic).add(config);
