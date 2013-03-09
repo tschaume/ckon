e6d3e4f5b5333781f0d0dcf1c23cd01a3364d9d0
Patrick Huck <phuck@lbl.gov>
2013-03-09 14:02:12 -0800

    cmdline: fix ckon_boost cin

--- src/cmdline/cmdline.cc
+++ src/cmdline/cmdline.cc
@@ -42,8 +42,8 @@ void cmdline::runSetup() {
 
   if ( utils::askYesOrNo("are you going to use boost") == 'y' ) {
     // set ckon_boost
-    cout << "space-separate list of boost library names:" << endl;
-    std::cin >> ckon_boost; // needs fixing
+    cout << "space-separate list of boost library names:";
+    std::getline(std::cin, ckon_boost);
     // download boost m4 files if not exist (don't remove old ones)
     //
     // FYI message
