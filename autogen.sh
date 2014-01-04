9a12a2fb394bff818c8522d9ec8c0b4a3df182cf
Patrick Huck <phuck@lbl.gov>
2014-01-03 18:27:05 -0800

    use ax_lib_curl.m4 from autoconf-archive

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 VERSION=0.6.3
+MIN_BOOST_VERSION=1.50
+MIN_CURL_VERSION=
 
 [ -e configure.ac ] && rm -v configure.ac
 
@@ -11,12 +13,15 @@ echo 'm4_pattern_allow([AM_PROG_AR])' >> configure.ac
 echo 'm4_ifdef([AM_PROG_AR], [AM_PROG_AR])' >> configure.ac
 echo 'AM_INIT_AUTOMAKE([-Wall no-define])' >> configure.ac
 echo 'AC_PROG_CXX' >> configure.ac
-echo 'AX_BOOST_BASE([1.50])' >> configure.ac
+echo 'AX_BOOST_BASE(['$MIN_BOOST_VERSION'])' >> configure.ac
 echo 'AX_BOOST_SYSTEM' >> configure.ac
 echo 'AX_BOOST_FILESYSTEM' >> configure.ac
 echo 'AX_BOOST_REGEX' >> configure.ac
 echo 'AX_BOOST_PROGRAM_OPTIONS' >> configure.ac
 echo 'LIBCURL_CHECK_CONFIG' >> configure.ac
+echo 'AX_LIB_CURL(['$MIN_CURL_VERSION'],,[' >> configure.ac
+echo '  AC_MSG_ERROR([You need libcurl >= '$MIN_CURL_VERSION'])' >> configure.ac
+echo '])' >> configure.ac
 echo 'AC_CONFIG_FILES([Makefile])' >> configure.ac
 echo 'AC_OUTPUT' >> configure.ac
 
@@ -25,14 +30,14 @@ echo 'AC_OUTPUT' >> configure.ac
 echo 'AUTOMAKE_OPTIONS = foreign subdir-objects -Wall' >> Makefile.am
 echo 'ACLOCAL_AMFLAGS = ${ACLOCAL_FLAGS} -I m4' >> Makefile.am
 echo 'AM_CPPFLAGS = $(BOOST_CPPFLAGS)' >> Makefile.am
-echo 'AM_CPPFLAGS += @LIBCURL_CPPFLAGS@' >> Makefile.am
+echo 'AM_CPPFLAGS += $(CURL_CFLAGS)' >> Makefile.am
 echo 'bin_PROGRAMS = ckon' >> Makefile.am
 echo 'ckon_LDADD = $(BOOST_LDFLAGS) -lstdc++' >> Makefile.am
 echo 'ckon_LDADD += $(BOOST_SYSTEM_LIB)' >> Makefile.am
 echo 'ckon_LDADD += $(BOOST_FILESYSTEM_LIB)' >> Makefile.am
 echo 'ckon_LDADD += $(BOOST_REGEX_LIB)' >> Makefile.am
 echo 'ckon_LDADD += $(BOOST_PROGRAM_OPTIONS_LIB)' >> Makefile.am
-echo 'ckon_LDADD += @LIBCURL@' >> Makefile.am
+echo 'ckon_LDADD += $(CURL_LIBS)' >> Makefile.am
 echo 'ckon_SOURCES =' >> Makefile.am
 for file in `find . -type f -name "*.h" -o -name "*.cc"`; do
   echo "ckon_SOURCES += $file" >> Makefile.am
