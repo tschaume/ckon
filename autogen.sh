96bf0eb0a92d9e6112753eea871ffe6d3bcb88f6
Patrick Huck <phuck@lbl.gov>
2014-01-03 22:09:01 -0800

    use AX_LIB_CURL from autoconf-archive

--- autogen.sh
+++ autogen.sh
@@ -7,9 +7,9 @@ MIN_CURL_VERSION=7.34.0
 [ -e configure.ac ] && rm -v configure.ac
 
 echo 'AC_INIT([ckon], ['$VERSION'])' >> configure.ac
+echo 'AC_CONFIG_MACRO_DIR([m4])' >> configure.ac
 echo 'm4_ifdef([AM_SILENT_RULES], [AM_SILENT_RULES([yes])])' >> configure.ac
 echo 'AC_CONFIG_AUX_DIR(config)' >> configure.ac
-echo 'm4_pattern_allow([AM_PROG_AR])' >> configure.ac
 echo 'm4_ifdef([AM_PROG_AR], [AM_PROG_AR])' >> configure.ac
 echo 'AM_INIT_AUTOMAKE([-Wall no-define])' >> configure.ac
 echo 'AC_PROG_CXX' >> configure.ac
@@ -18,10 +18,8 @@ echo 'AX_BOOST_SYSTEM' >> configure.ac
 echo 'AX_BOOST_FILESYSTEM' >> configure.ac
 echo 'AX_BOOST_REGEX' >> configure.ac
 echo 'AX_BOOST_PROGRAM_OPTIONS' >> configure.ac
-echo 'LIBCURL_CHECK_CONFIG' >> configure.ac
-echo 'AX_LIB_CURL(['$MIN_CURL_VERSION'],,[' >> configure.ac
-echo '  AC_MSG_ERROR([You need libcurl >= '$MIN_CURL_VERSION'])' >> configure.ac
-echo '])' >> configure.ac
+echo -n "AX_LIB_CURL([$MIN_CURL_VERSION],," >> configure.ac
+echo "AC_MSG_ERROR([You need libcurl >= $MIN_CURL_VERSION]))" >> configure.ac
 echo 'AC_CONFIG_FILES([Makefile])' >> configure.ac
 echo 'AC_OUTPUT' >> configure.ac
 
