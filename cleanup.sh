8abe61e8cd9b18ade9e922bbcb0c38c590d45b6b
Patrick Huck <phuck@lbl.gov>
2014-01-09 00:19:27 -0800

    improve cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-set -v
-rm -rf build
-rm Makefile.in
-rm configure
-rm aclocal.m4
-rm -rf autom4te.cache
-rm -rf config
-find . -type f -name ".deps" | xargs rm
+#set -v
+# files
+[ -e Makefile.in ] && rm -v Makefile.in
+[ -e configure ] && rm -v configure
+[ -e aclocal.m4 ] && rm -v aclocal.m4
+# dirs
+[ -d build ] && rm -rvf build
+[ -d autom4te.cache ] && rm -rvf autom4te.cache
+[ -d config ] && rm -rvf config
+[ -d bin ] && rm -rvf bin
+find . -type f -name ".deps" -exec rm -v {} \;
